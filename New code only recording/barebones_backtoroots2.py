911a35cb7da58987ba9a3b14a79267d1433dde7d
heizalo <89796977+heizalo@users.noreply.github.com>
2023-04-23 21:50:37 -0500

    Update barebones_backtoroots2.py

--- New code only recording/barebones_backtoroots2.py
+++ New code only recording/barebones_backtoroots2.py
@@ -6,7 +6,8 @@ def start_recording():
     
     print("Starting to record...")
     storage_location = "/mnt/usb1/"
-    # Generate a unique filename for the output video file
+    
+    # filename includes time and date
     timestamp = time.strftime("%Y-%m-%d_%H-%M-%S", time.localtime())
     filename = f"{timestamp}.h264"
 
@@ -23,13 +24,13 @@ def start_recording():
     # Record for in secods 60 sec = 1 min
     camera.wait_recording(60)
 
-    # Stop recording and release resources
+    # Stop recording and release camera, camera.close has to be set so it can record once again
     camera.stop_recording()
     camera.close()
 
     print("Recording has stopped.")
 
-# Schedule "day" can be replaced with monday, tues...
+# Schedule "day" can be replaced with monday, tues... these can be removed if only 1 day per week needs to be active
 schedule.every().monday.at("19:12").do(start_recording)
 schedule.every().tuesday.at("19:12").do(start_recording)
 schedule.every().wednesday.at("19:12").do(start_recording)
@@ -39,7 +40,7 @@ schedule.every().friday.at("19:12").do(start_recording)
 
 start_recording()
 
-# Run the scheduling loop
+# while loop running scheduled script
 while True:
     schedule.run_pending()
     time.sleep(1)
