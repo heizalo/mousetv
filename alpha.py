5d7d22b359080d0e7a1a0a1f3bcb3230029c0bda
heizalo <89796977+heizalo@users.noreply.github.com>
2023-04-25 17:47:35 -0500

    Update alpha.py

--- alpha.py
+++ alpha.py
@@ -10,38 +10,38 @@ import datetime
 #if not recording then skip 
 
 def start_recording(should_record):
-    if not should_record:
+    if should_record == false:
         return
 
-    print("Starting recording...")
+    print("Starting to record...")
     
     storage_location = "/mnt/usb1/"
     filename = storage_location + datetime.datetime.now().strftime("%Y%m%d__%H-%M-%S ") + ".h264"
   
 
-    # Initialize PiCamera object
+    # Initialize PiCamera
     camera = PiCamera()
 
     # Set camera resolution and framerate
     camera.resolution = (640, 480)
     camera.framerate = 20
 
-    # Start recording
+    # Start recording + filename which is the date and time
     camera.start_recording(filename)
 
-    # Record for 60 seconds
+    # Record for x seconds
     camera.wait_recording(60)
 
     # Stop recording and release resources
     camera.stop_recording()
     camera.close()
 
-    print("Recording stopped.")
+    print("Recording has stopped.")
 
 # Schedule "day" can be replaced with monday, tues...
 schedule.every().day.at("20:45").do(start_recording, True)
 
-# Run the scheduling loop
+# running loop
 while True:
     schedule.run_pending()
     time.sleep(1)
